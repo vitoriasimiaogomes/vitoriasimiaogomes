c0af43d59dfb0f0f233aa3eababbf38a8118e92c
vitoriasimiaogomes <126607544+vitoriasimiaogomes@users.noreply.github.com>
2023-06-06 21:16:26 -0300

    Add files via upload

---
+++ arquivo github karol.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def soma (a, b):
+    return a + b
+
+
+def quadrado (a):
+    return a **2
+    
+
+def hipotenusa ( cateto1, cateto2 ):
+    return soma( quadrado(cateto1 ) , quadrado( cateto2 ) ) ** ( 1 / 2 )
+
+print (soma(2,3))
+
+# estruturas de decisão 
+
+def simples(cor):
+    if cor == 'azul':
+        return 'escolheu certo'
+
+def medio (cor):
+    if cor == 'azul':
+        return 'escolheu certo'
+    else:
+        return 'tente outra vez'
+    
+    def completo (cor):
+        if cor == 'azul':
+            return 'escolheu certo' 
+        elif cor == 'marrom' :
+            return 'não tem salvação'
+        else:
+            return 'tente outra vez'
+        
+    
